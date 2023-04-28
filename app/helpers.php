2e08f5c6c61b7f102c3290f518f83f77629f41eb
Seryak <misha-seryak@ya.ru>
2023-04-29 00:46:51 +0300

    UPD .env.example remove useless
    feat: new path function for content/ folder

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,15 @@
 <?php
 
-if (! function_exists('test')) {
-    function test() {
+if (!function_exists('test')) {
+    function test()
+    {
         dd('test');
+    }
+}
+
+if (!function_exists('content_path')) {
+    function content_path(string $path): string
+    {
+        return base_path('content/' . $path);
     }
 }
