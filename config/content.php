050a095e53c8204de5f5e850c895335598cbf881
Seryak <misha-seryak@ya.ru>
2023-05-05 22:26:34 +0300

    feat | Add output path file env parameter for html suffix

--- config/content.php
+++ config/content.php
@@ -1,12 +1,24 @@
 <?php
 
 return [
-    'pages' => [
-        'sorting' => false,
-        'path' => '{filename}',
-        'items' => [
-            content_path('hello.md'),
-            content_path('test.md'),
+    'use_html_suffix' => env('USE_HTML_SUFFIX', false),
+    'output_directory' => env('OUTPUT_DIRECTORY', 'build'),
+    'collections' => [
+        'pages' => [
+            'sorting' => false,
+            'path' => '{filename}',
+            'items' => [
+                content_path('hello.md'),
+                content_path('test.md'),
+            ],
         ],
-    ],
+        'jetbrains-posts' => [
+            'sorting' => false,
+            'path' => 'blog/jetbrains/{filename}',
+            'items' => [
+                content_path('hello.md'),
+                content_path('test.md'),
+            ],
+        ],
+    ]
 ];
